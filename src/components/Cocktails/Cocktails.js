c468ce33a81d834e474c17d26593ff1a77f0ddd0
Mark R. Collins <markcollins.md@gmail.com>
2022-01-20 06:54:44 -0700

    Set up Cocktail to dynamically display component based on state

--- src/components/Cocktails/Cocktails.js
+++ src/components/Cocktails/Cocktails.js
@@ -9,21 +9,31 @@ class Cocktails extends Component {
 
     this.state = {
       randomizedCocktails: [],
-      toDisplay: ''
+      toDisplay: 'loading'
     }
   }
 
   componentDidMount() {
     let shuffledCocktails = cocktailData.sort( () => Math.random() - 0.5);
     let randomizedCocktails = shuffledCocktails.slice(0, 4)
-    this.setState({ randomizedCocktails })
+    this.setState({ randomizedCocktails, toDisplay: 'menu' })
+  }
+
+  onDisplay = () => {
+    switch (this.state.toDisplay) {
+      case 'loading':
+        return <div>Loading</div>
+      case 'menu':
+        return <Menu randomCocktails ={ this.state.randomizedCocktails }/>
+      default:
+        break;
+    }
   }
 
   render() {
 
-
     return <section className="cocktail-display">
-      <Menu  randomCocktails ={ this.state.randomizedCocktails }/>
+      { this.onDisplay() }
       </section>
   }
 }
