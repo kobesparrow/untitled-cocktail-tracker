6d22c6fe584c98dcf39c83d9826e5c5c73a376ff
Mark R. Collins <markcollins.md@gmail.com>
2022-01-21 22:01:22 -0700

    CocktailDetail component showing correct cocktail based on click

--- src/components/CocktailDetail/CocktailDetail.js
+++ src/components/CocktailDetail/CocktailDetail.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const CocktailDetail = () => {
+const CocktailDetail = ({ cocktail }) => {
 
-  return <article>
-      <p>Cocktail Detail here!</p>
+  const testFunc = () => {
+    console.log('test', cocktail)
+  }
+
+
+
+  return <article className="cocktail-detail">
+      <div className="left-column">
+        <p>This is the picture of the glass</p>
+        <p>This is the description</p>
+        <div>These are the ingredients</div>
+      </div>
+      <div className="right-column">
+        <p>{ cocktail.cocktailName }</p>
+        <p>This is the rating</p>
+        <p>This is the date last poured for the user</p>
+        <p>This is the total number drank</p>
+        <p>These are tasting notes</p>
+      </div>
     </article> 
 }
 
