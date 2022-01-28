8f80436e5f2cb6c0bfe3ce1ca3a6d27019ae734b
Mark R. Collins <markcollins.md@gmail.com>
2022-01-28 07:48:43 -0700

    Most recent user drinks consumed showing in user section

--- src/components/UserDisplay/UserDisplay.js
+++ src/components/UserDisplay/UserDisplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import userData from '../../data/userDataSet';
+import cocktailData from '../../data/cocktailDataSet';
 import Mando from '../../images/mando-helmet.png';
 
 class UserDisplay extends Component {
@@ -9,7 +10,40 @@ class UserDisplay extends Component {
   }
 
   totalCocktailsPoured = () => {
-    console.log(userData[0].userName)
+    // console.log(userData[0].userName)
+  }
+
+  displayUserDrinkHistory = () => {
+    let recentDrinks = [];
+    
+    userData[0].cocktails.forEach(cocktail => {
+      recentDrinks.push(cocktailData.find(data => data.cocktailName === cocktail.cocktailName))
+    })
+
+    // return recentDrinks.map(drink => {
+    //   return <div>
+    //       <p></p>
+    //       <p></p>
+    //     </div>
+    // })
+
+
+    let bigAssReduce = userData[0].cocktails.reduce((acc, cocktail) => {
+      //get date consumed from userData
+      //user cocktail name from userData to get cocktail info from cocktailData
+      //combine those two into a <div>
+      //push that div into the acc
+
+      let drinkData = cocktailData.find(data => data.cocktailName === cocktail.cocktailName)
+      let recentDrinkDiv = <div>
+          <p>{ drinkData.cocktailName }</p>
+          <p>Date Consumed: { cocktail.mostRecent }</p>
+        </div>
+      acc.push(recentDrinkDiv)
+      return acc;
+    }, [])
+
+    return bigAssReduce
   }
 
   render() {
@@ -19,6 +53,7 @@ class UserDisplay extends Component {
         <p>Cocktails poured: {this.totalCocktailsPoured()}</p>
         <p>Percent complete:</p>
         <p>Recent Drink History:</p>
+        { this.displayUserDrinkHistory() }
       </article>
   }
 }
