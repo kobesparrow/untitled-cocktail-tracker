0fded1aea4e1169abfa4770e8981bef813c59d39
Mark R. Collins <markcollins.md@gmail.com>
2022-02-06 12:14:28 -0700

    cleaned up unused lines of code on Cocktail component

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -12,12 +12,6 @@ class Cocktail extends Component {
     }
   }
 
-  // componentDidMount = () => {
-  //   //set to load average user if no currentUser available
-  //   let userCocktail = this.props.currentUser.cocktails.find(cocktail => cocktail.cocktailName === cocktail.cocktailName)
-  //   this.setState({ userCocktail })
-  // }
-
   displayHelper = (display) => {
     this.setState({ display })
   }
@@ -49,22 +43,17 @@ class Cocktail extends Component {
     })
   }
  
-  leftColumnDisplay = () => {
-    switch (this.state.display) {
-      case 'main':
-        return <div className="horizontal-cocktail-ingredients">
-              {/* { this.horizontalDisplayIngredients() } */}
-            </div>
-      case 'mix':
-        return null
-      default:
-        break;
-    };
-  }
-
-  // starRatingDisplay = () => {
-  //   let stars = [];
-
+  // leftColumnDisplay = () => {
+  //   switch (this.state.display) {
+  //     case 'main':
+  //       return <div className="horizontal-cocktail-ingredients">
+  //             {/* { this.horizontalDisplayIngredients() } */}
+  //           </div>
+  //     case 'mix':
+  //       return null
+  //     default:
+  //       break;
+  //   };
   // }
 
   testFunction = () => {
@@ -77,8 +66,6 @@ class Cocktail extends Component {
         return <div>loading</div>
       case 'main':
         return <div>
-            {/* <p>Rating: { this.props.userCocktail.rating }</p> */}
-            {/* <p>Last poured: { this.props.userCocktail.mostRecent }</p> */}
             { this.verticalDisplayIngredients() }
             { this.displayInstructions() }
             <button onClick={ () => this.displayHelper('mix') }>Mix</button>
@@ -93,20 +80,11 @@ class Cocktail extends Component {
               <img src={ Star } className="rating-star" id="4" onClick={ this.props.rateCocktail } />
               <img src={ Star } className="rating-star" id="5" onClick={ this.props.rateCocktail } />
             </div>
-            { this.verticalDisplayIngredients() }
-            { this.displayInstructions() }
-          </div>
-      case 'pour':
-        return <div>
-            <p>Rate your cocktail:</p>
-            <div>
-              <img src={ Star } className="rating-star" id="1" onClick={ this.props.rateCocktail } />
-              <img src={ Star } className="rating-star" id="2" onClick={ this.props.rateCocktail } />
-              <img src={ Star } className="rating-star" id="3" onClick={ this.props.rateCocktail } />
-              <img src={ Star } className="rating-star" id="4" onClick={ this.props.rateCocktail } />
-              <img src={ Star } className="rating-star" id="5" onClick={ this.props.rateCocktail } />
+            <div className="horizontal-cocktail-ingredients">
+              { this.horizontalDisplayIngredients() }
             </div>
-          </div>    
+            { this.displayInstructions() }
+          </div>   
       default:
         break;
     };
@@ -120,7 +98,7 @@ class Cocktail extends Component {
               <p>This is the description</p>
               <p>Total Drank: { this.testFunction() }</p>
               <p>These are tasting notes</p>
-              { this.leftColumnDisplay() }
+              {/* { this.leftColumnDisplay() } */}
             </div>
             <div className="cocktail-display--right-column">
               <p className="header">{ this.props.cocktail.cocktailName }</p>
